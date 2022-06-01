0d58bfcbc0f0f308a6849afa42e2673969d38a35
Mika Angeles <angelesmika8@gmail.com>
2022-06-01 23:12:49 +0800

    level 2 ?

--- sender.py
+++ sender.py
@@ -9,25 +9,63 @@ import hashlib
 ID = '881967a8'
 HOST = '10.0.7.141'
 PORT = 6670
-ADDR = (HOST, PORT)
-ADDR2 = (HOST, 9000)
+SRC_ADDR = (HOST, PORT)
+DST_ADDR = (HOST, 9000)
+HOST_NAME = socket.gethostbyname(socket.gethostname())
 
 # Initiate UDP connection
 UDP_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 # Open payload.txt
-FILE = open('payload.txt')
+FILE = open('881967a8.txt')
 
+# Checksum function
 def checksum(packet):
     return hashlib.md5(packet.encode('utf-8')).hexdigest()
 
+# Initialize main function
 def main():
-    UDP_SOCKET.bind((socket.gethostbyname(socket.gethostname()), PORT))
-    UDP_SOCKET.sendto(f'ID{ID}'.encode(), ADDR2)
 
+    # Bind the UDP socket to host and port
+    UDP_SOCKET.bind((HOST_NAME, PORT))
+
+    # Initiate a transaction and send ID
+    UDP_SOCKET.sendto(f'ID{ID}'.encode(), DST_ADDR)
+
+    # Receive packets and print transaction ID
     data, addr = UDP_SOCKET.recvfrom(1024)
-    transaction = data.decode()
-    print(transaction)
+    transactionID = data.decode()
+    print(transactionID)
+
+    # Load and read the payload from file
+    payload = FILE.read()
+    payloadSize = len(payload)
+
+    idx = 0
+    last = 0
+    seqNum = 0
+    msgLen = max(1, payloadSize)
+
+    while idx < payloadSize:
+        while True:
+            msg = payload[idx:payloadSize-1] if idx + msgLen == payloadSize else payload[idx:idx + msgLen]
+            packet = f'ID{ID}SN{str(seqNum).zfill(7)}TXN{transactionID}Z{str(last)}{msg}'
+
+            UDP_SOCKET.sendto(packet.encode(), DST_ADDR)
+            print(f"Packet sent:{packet}")
+
+            data, addr = UDP_SOCKET.recvfrom(1024)
+            ACK = data.decode()
+            print(f"Checksum: {ACK}")
+
+            CHECKSUM = checksum(packet)
+            if ACK == CHECKSUM:
+                print(">> Checksums match!")
+                break
+        
+        seqNum += 1
+        idx += msgLen
+        print()
 
 if __name__ == "__main__":
     main()
