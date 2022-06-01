01d766add4575ffcbc9dd450222be6bd3c0e8273
Mika Angeles <71173355+angelesmika@users.noreply.github.com>
2022-06-01 18:55:03 +0800

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,28 @@
+# Angeles, Ma. Mikaela A.
+# 201800527
+# CS 145 Project
+
+import socket
+import hashlib
+
+ID = '881967a8'
+HOST = '10.0.7.141'
+PORT = 6670
+ADDR = (HOST, PORT)
+
+UDP_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+FILE = open('payload.txt')
+
+def checksum(packet):
+    return hashlib.md5(packet.encode('utf-8')).hexdigest()
+
+def main():
+    UDP_SOCKET.bind(('', PORT))
+    UDP_SOCKET.sendto(f'ID{ID}'.encode(), ADDR)
+
+    data, addr = UDP_SOCKET.recvfrom(1024)
+    message = data.decode()
+    print(message)
+
+if __name__ == "__main__":
+    main()
