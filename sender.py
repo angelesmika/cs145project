db0e3337591bac8536bbbd7f76a9cc90a3cbe7ec
Mika Angeles <angelesmika8@gmail.com>
2022-06-03 00:23:00 +0800

    Fix

--- sender.py
+++ sender.py
@@ -10,6 +10,10 @@ import socket
 import hashlib
 import argparse
 
+# Initiate UDP connection and set a timeout
+UDP_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+UDP_SOCKET.settimeout(10)
+
 # Checksum function
 # Given in the project specifications
 def checksum(packet):
@@ -29,6 +33,39 @@ def parse_input(str):
 
     return cmd
 
+def get_max_payload_size(ID, TID, DEST, payload):
+    idx = 0
+    seq_num = 0
+    payloadSize = len(payload)
+
+    # Assume that 10% of the payload can be sent on the first try
+    msg_len = max(1, math.ceil(int(payloadSize * 0.90)))
+
+    # While the packet is not being sent, remove 5% of the payload
+    # until the maximum acceptable packet size is obtained
+    while (idx < payloadSize):
+        msg = payload[idx : msg_len + idx] if idx + msg_len < payloadSize else payload[idx : payloadSize - 1]
+
+        z = 0 if idx + msg_len < payloadSize else 1
+        packet = f"ID{ID}SN{str(seq_num).zfill(7)}TXN{TID}LAST{str(z)}{msg}"
+        print(f"Message: {packet}")
+
+        # Obtain the checksum of the packet
+        CHECKSUM = checksum(packet)
+
+        # Send the packet to the server and check if it returns an error
+        # If an error is recorded, decrease msg_len by 5% and try sending
+        # the packet again until the server validates the packet
+        UDP_SOCKET.sendto(packet.encode(), DEST)
+        try:
+            ACK = UDP_SOCKET.recv(64).decode()
+        except socket.error or socket.timeout:
+            msg_len = int(msg_len * 0.95)
+            continue
+
+        if ACK[-32:] == CHECKSUM:
+            print(f">> Checksums match! Maximum payload size is {msg_len}")
+
 def main():
     cmd = parse_input(sys.argv[1:])   # Parse user input in the terminal
 
@@ -42,8 +79,8 @@ def main():
     # Print variables
     print(f">> Commands: -i: {ID} | -f: {FILE} | -a: {ADDR} | -s: {RCV_PORT} | -c: {SEND_PORT}")
 
-    UDP_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)    # Initiate UDP connection
-    HOST_NAME = socket.gethostbyname(socket.gethostname())          # Get host name
+    # Get host name
+    HOST_NAME = socket.gethostbyname(socket.gethostname())
 
     DST_ADDR = (ADDR, RCV_PORT)                # Initialize the address to be used
     UDP_SOCKET.bind((HOST_NAME, SEND_PORT))    # Bind the UDP socket to host and port
@@ -52,8 +89,11 @@ def main():
     UDP_SOCKET.sendto(f'ID{ID}'.encode(), DST_ADDR)
 
     # Receive packets and print transaction ID
-    transactionID = UDP_SOCKET.recv(64).decode()
-    print(f"Transaction ID: {transactionID}")
+    TID = UDP_SOCKET.recv(64).decode()
+    print(f"Transaction ID: {TID}")
+
+    payload = open(FILE).read()
+    get_max_payload_size(ID, TID, DST_ADDR, payload)
 
 if __name__ == "__main__":
     main()
