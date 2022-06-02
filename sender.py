ead0be746c1b509b05e78b181530c6dda26d448e
Mika Angeles <angelesmika8@gmail.com>
2022-06-02 23:00:11 +0800

    Level 1

--- sender.py
+++ sender.py
@@ -2,93 +2,69 @@
 # 201800527
 # CS 145 Project
 
+# Import libraries
 import sys
 import math
 import time
 import socket
+import getopt
 import hashlib
 
-# Initialize variables
+# Initialize default variables
 ID = '881967a8'
-HOST = '10.0.7.141'
-PORT = 6670
-SRC_ADDR = (HOST, PORT)
-DST_ADDR = (HOST, 9000)
+FILE = '881967a8.txt'
+ADDR = '10.0.7.141'
+RCV_PORT = 9000
+SEND_PORT = 6670
 HOST_NAME = socket.gethostbyname(socket.gethostname())
 
-# Initiate UDP connection
-UDP_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-# Open payload.txt
-FILE = open('881967a8.txt')
-
 # Checksum function
+# Given in the project specifications
 def checksum(packet):
     return hashlib.md5(packet.encode('utf-8')).hexdigest()
 
-# Initialize main function
-def main():
-    # Print the arguments passed from the command line
-    print(f"> Number of arguments: {len(sys.argv)}")
+# Parse user input in the terminal using getopt library
+# Source: https://www.geeksforgeeks.org/getopt-module-in-python/
+def parse_input(str):
+    global FILE, ADDR, RCV_PORT, SEND_PORT, ID
+    try:
+        flags, args = getopt.getopt(str, "f:a:s:c:i:")
+    except:
+        return ">> No additional flags found."
 
-    print("\n======== ARGUMENTS ========")
-    for arg in sys.argv:
-        print("> " + arg)
-    print("===========================\n")
+    for flag, arg in flags:
+        if flag in ['-f']:      # -f denotes the filename of the payload
+            FILE = flag
+        elif flag in ['-a']:    # -a denotes the IP address of the receiver to be contacted
+            ADDR = flag
+        elif flag in ['-s']:    # -s denotes the port to be used by the receiver
+            RCV_PORT = flag
+        elif flag in ['-c']:    # -c denotes the port to be used by the sender
+            SEND_PORT = flag
+        elif flag in ['-i']:    # -i denotes the unique ID
+            ID = flag
+
+    return
+
+def main():
+    # Parse the user input in the terminal
+    parse_input(sys.argv[1:])
+
+    # Initiate UDP connection
+    UDP_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    # Initialize the address to be used
+    DST_ADDR = (ADDR, SEND_PORT)
 
     # Bind the UDP socket to host and port
-    UDP_SOCKET.bind((HOST_NAME, PORT))
-
-    # Set a timeout for the socket
-    UDP_SOCKET.settimeout(10)
+    UDP_SOCKET.bind((HOST_NAME, RCV_PORT))
 
     # Initiate a transaction and send ID
     UDP_SOCKET.sendto(f'ID{ID}'.encode(), DST_ADDR)
 
     # Receive packets and print transaction ID
-    data, addr = UDP_SOCKET.recvfrom(64)
-    transactionID = data.decode()
+    transactionID = UDP_SOCKET.recv(64).decode()
     print(f"Transaction ID: {transactionID}")
-
-    # Load and read the payload from file
-    payload = FILE.read()
-    payloadSize = len(payload)
-    print(f"> Payload size: {payloadSize}")
-
-    idx = 0
-    seqNum = 0
-    msgLen = max(1, math.ceil(payloadSize / 90))
-
-    for i in range(0, payloadSize):
-        start = time.time()
-
-        while True:
-            print(F"> Message length: {msgLen}")
-            last = 0 if idx + msgLen < payloadSize else 1
-
-            msg = payload[idx:idx+msgLen] if idx + msgLen < payloadSize else payload[idx:]
-            packet = f"ID{ID}SN{str(seqNum).zfill(7)}TXN{transactionID}LAST{str(last)}{msg}"
-            
-            CHECKSUM = checksum(packet)
-
-            UDP_SOCKET.sendto(packet.encode(), DST_ADDR)
-            try:
-                ACK = UDP_SOCKET.recv(64).decode()
-                print(f"Packet sent: {packet}")
-            except socket.error or socket.timeout:
-                msgLen = int(msgLen * 0.90)
-                continue
-            
-            if ACK[-32:] == CHECKSUM:
-                print(">> Checksums match!")
-                break
-        
-        end = time.time()
-        UDP_SOCKET.settimeout(end - start + 5)
-
-        seqNum += 1
-        idx += msgLen
-        print()
 
 if __name__ == "__main__":
     main()
