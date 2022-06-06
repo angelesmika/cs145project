897a63949cca188181a4bbd67486ab42d852b8df
Mika Angeles <angelesmika8@gmail.com>
2022-06-06 09:48:47 +0800

    ix

--- sender.py
+++ sender.py
@@ -15,6 +15,7 @@ timeout = 15
 
 # Initiate UDP connection
 UDP_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+UDP_SOCKET.settimeout(timeout)
 
 # Checksum function
 # Given in the project specifications
@@ -56,6 +57,7 @@ def get_payload_size(ID, TID, DEST, payload):
         try:
             ACK = UDP_SOCKET.recv(64).decode()
         except socket.error:
+            print(f">> {UDP_SOCKET.gettimeout()}")
             msg_len = int(msg_len * 0.85)
             continue
         
@@ -121,7 +123,6 @@ def main():
     SN = 1          # Sequence number
     idx = msg_len   # Index to be accessed in the payload
 
-    UDP_SOCKET.settimeout(timeout)      # Set timeout to the socket
     while idx < payload_len:
         # Get the (cumulative) length of the payload sent
         sent = idx + msg_len if idx + msg_len < payload_len else payload_len - 1
